e7edf4ad8fea8134f93a46690542a7365adceced
最后、結束 <13671683@qq.com>
2019-06-06 14:34:35 +0800

    666

--- src/main/java/com/newer/finance/Application.java
+++ src/main/java/com/newer/finance/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.newer.finance.mapper")
 public class Application {
-
+///wwwwwwwwwwwwwwwwwwwwww
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
