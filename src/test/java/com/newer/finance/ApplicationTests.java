30e0abb2680912e18845d3020da3653a5b4f829b
最后、結束 <13671683@qq.com>
2019-06-06 15:22:37 +0800

    wangwei 冒泡

--- src/test/java/com/newer/finance/ApplicationTests.java
+++ src/test/java/com/newer/finance/ApplicationTests.java
@@ -11,6 +11,29 @@ public class ApplicationTests {
 
     @Test
     public void contextLoads() {
+        int[] all={1,3,5,10,12,19,7};
+        //冒泡前数组
+        for (int num:all){
+            System.out.println("冒泡前数组"+num);
+        }
+        for (int i=0;i<all.length-1;i++){
+            for (int j=0;j<all.length-1-i;j++){
+                //大放右
+if (all[j]>all[j+1]){
+    int temp=all[j];
+    all[j]=all[j+1];
+    all[j+1]=temp;
+}
+                }
+            }
+       for (int num:all){
+           System.out.println(num);
+       }
+       
+        }
+
+
     }
 
-}
+
+
